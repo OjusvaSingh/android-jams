4f08792c78d5c8f4fd2782563202744504616ed9
OjusvaSingh <75851617+OjusvaSingh@users.noreply.github.com>
2020-12-18 19:00:43 +0530

    Create q2.kt

---
+++ week1/q2.kt
@@ -0,0 +1,15 @@
+fun main() {
+  val a=1
+  val b=2  
+  val c=3
+  var max:Int
+    
+    if(a>b && a>c)
+    max=a
+    else if (b>a &&b>c)
+    max=b
+    else
+    max=c
+    
+    println("Maximum is "+max)
+}
