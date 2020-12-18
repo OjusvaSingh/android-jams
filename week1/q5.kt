27d24e0cfb38c77567f5b4089a17f3ddcb467d2b
OjusvaSingh <75851617+OjusvaSingh@users.noreply.github.com>
2020-12-18 19:24:26 +0530

    Create q5.kt

---
+++ week1/q5.kt
@@ -0,0 +1,11 @@
+fun main() {
+ val a=2
+ val b=3
+ val c=1
+    if(a==b && a==c)
+    println("Equilateral triangle")
+    else if(a!=b && a!=c && b!=c)
+    println("Scalene triangle")
+    else
+    println("Isosceles triangle")   
+}
